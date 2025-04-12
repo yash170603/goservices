926ccb49be484095eba9c8761a1603936cddfd57
yash_talreja <yashtalreja180@gmail.com>
2025-04-12 16:17:40 +0530

    first push working with g-ql

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/yash170603/goservices
+
+go 1.23.0
+
+toolchain go1.23.7
+
+require (
+	github.com/99designs/gqlgen v0.17.68
+	github.com/vektah/gqlparser/v2 v2.5.23
+)
+
+require (
+	github.com/agnivade/levenshtein v1.2.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/sosodev/duration v1.3.1 // indirect
+)
---
+++ graphql/graph.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"github.com/99designs/gqlgen/graphql"
+	"github.com/yash170603/goservices/account"
+	"github.com/yash170603/goservices/catalog"
+	"github.com/yash170603/goservices/order"
+)
+
+type Server struct {
+	accountClient *account.Client
+	catalogClient *catalog.Client
+	orderClient   *order.Client
+}
+
+func newGraphqlServer(accountUrl, catalogUrl, orderUrl string) (*Server, error) {
+
+	var _ = graphql.HandlerExtension(nil)
+	accountClient, err := account.NewClient(accountUrl)
+	if err != nil {
+		return nil, err
+	}
+
+	//catalog is dependent on accountClient
+	catalogClient, err := catalog.NewClient(catalogUrl)
+	if err != nil {
+		accountClient.close()
+		return nil, err
+	}
+
+	// the ordeClient is both dependetn on catalogClient and accountClient
+	orderClient, err := order.NewClient(orderUrl)
+	if err != nil {
+		catalogClient.Close()
+		accountClient.close()
+		return nil, err
+	}
+
+	return &Server{
+		accountClient: accountClient,
+		catalogClient: catalogClient,
+		orderClient:   orderClient,
+	}, nil
+}
+
+func (s *Server) Mutation() MutationResolver {
+	return &mutationResolver{
+		server: s,
+	}
+}
