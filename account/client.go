27e00cab7214e95678063da68ab0768bba512bd3
yash_talreja <yashtalreja180@gmail.com>
2025-05-16 16:04:14 +0530

    dealt with some deprecated functions for grpc client conn, gotta check this later, not resolved

--- account/client.go
+++ account/client.go
@@ -1,1 +1,29 @@
 package account
+
+import (
+	"github.com/yash170603/goservices/account/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type Client struct {
+	conn    *grpc.ClientConn
+	service pb.AccountServiceClient
+}
+
+func NewClient(url string) (*Client, error) {
+	// Replace deprecated grpc.WithInsecure() with the newer approach
+	conn, err := grpc.NewClient(
+		url,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	client := pb.NewAccountServiceClient(conn)
+	return &Client{
+		conn,
+		client,
+	}, nil
+}
